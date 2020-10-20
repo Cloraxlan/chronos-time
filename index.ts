daa2c7cc6132f4b9612f90fceb270044e2fe58d8
Konrad Rozpadek <sbluek546@gmail.com>
2020-10-20 16:51:57 -0500

    got rid of test case

--- index.ts
+++ index.ts
@@ -1,23 +1,6 @@
-import { SchedualManager } from "./SchedualManager";
-
 export * from "./EmptySchedual";
 export * from "./ExternalSchedualLoader";
 export * from "./PassingTimeSlot";
 export * from "./Schedual";
 export * from "./SchedualManager";
 export * from "./TimeSlot";
-let s = new SchedualManager(
-  [
-    {
-      tags: ["a"],
-      defaultNextSchedualTag: "a",
-      outOfBoundsName: "a",
-      timeSlots: [{ begin: [12 + 5, 0], end: [12 + 6, 0], name: "test" }],
-    },
-  ],
-  () => {}
-);
-s.goToSchedual("a");
-setInterval(() => {
-  console.log(s.currentName, s.currentTimeLeft, s.nextName);
-}, 1000);
