c772ff89ec3097b203b8d33be9ebd941d8686080
Konrad Rozpadek <sbluek546@gmail.com>
2020-10-20 08:02:11 -0500

    built

--- js/index.d.ts
+++ js/index.d.ts
@@ -1,1 +1,6 @@
-export {};
+export * from "./EmptySchedual";
+export * from "./ExternalSchedualLoader";
+export * from "./PassingTimeSlot";
+export * from "./Schedual";
+export * from "./SchedualManager";
+export * from "./TimeSlot";
